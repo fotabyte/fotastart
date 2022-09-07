a73de28d039981e37a0e691fda118a54714991df
levizwannah <levizwannah@gmail.com>
2022-09-07 18:42:56 +0300

    started the framework

---
+++ public/js/global/classes/Dom.js
@@ -0,0 +1,56 @@
+/**
+ * The Dom Class provides shorted API for the document model that is available in the windows object.
+ */
+class Dom{
+    /**
+     * Returns a new DOM object. Add this to the window using `window.dom = new Dom()`;
+     */
+    constructor(){
+    }
+
+    /**
+     * Gets a single element
+     * @param {string} selector css selector
+     */
+    get(selector){
+        return document.querySelector(selector);
+    }
+
+    /**
+     * Gets all the elements
+     * @param {string} selector css selector
+     * @returns 
+     */
+    all(selector){
+        return document.querySelectorAll(selector);
+    }
+
+    /**
+     * Gets the first element from the selected node list
+     * @param {string} selector css selector
+     */
+    first(selector){
+        let list = this.all(selector);
+        return list.length == 0 ? null : list[0]
+    }
+
+    /**
+     * Gets the last element from the select node list
+     * @param {string} selector css selector
+     */
+    last(selector){
+        let list = this.all(selector);
+        return list.length == 0 ? null : list[list.length - 1];
+    }
+
+    /**
+     * Get element at a position
+     * @param {string} selector css selector
+     * @param {int} position 
+     * @returns 
+     */
+    at(selector, position){
+        let list = this.all(selector);
+        return list.length == 0 ? null : list[position];
+    }
+}
